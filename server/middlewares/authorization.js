10064a194e126cbf2f9fead643b84f58add40544
wirahadi007 <charlesotojr@gmail.com>
2021-10-13 01:14:51 +0800

    feat: fix-conflix

--- server/middlewares/authorization.js
+++
@@ -1,9 +0,0 @@
-const authorization = (roles) => (req,res,next) => {
-    const userRole = req.currentUser.role
-    if(!roles.includes(userRole)) {
-        return next({name: "Unauthorize"})
-    }
-    next()
-}
-
-module.exports = authorization
