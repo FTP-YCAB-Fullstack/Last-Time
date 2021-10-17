38bed5368ae4910c58f170ab5a01a53fd2eb685e
wirahadi007 <charlesotojr@gmail.com>
2021-10-17 13:37:37 +0800

    feat: deteksi sampah

--- client/src/components/auth/Input.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Input = ({name , type}) => {
-    return (
-        <input className="border-2 border-gray-200 block mt-2" name={name} type={type} />
-    )
-}
-
-export default Input
