317dade03b55024e648d3826f6b168d387144f88
ghabrielf <ghabrielfebryann@gmail.com>
2021-10-17 18:26:14 +0700

    feat:create service route

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -3,13 +3,15 @@ import Navbar from '../../components/home/Navbar'
 import Banner from '../../components/home/Banner'
 import About from '../../components/home/About'
 import Footer from '../../components/home/Footer'
+import Service from '../../components/home/Service'
 
 const index = () => {
     return (
-        <div className="pt-14">
+        <div className="pt-14 dark:bg-gray-800 text-gray-600 dark:text-gray-100">
             <Navbar />
             <Banner />
             <About />
+            <Service />
             <Footer />
         </div>
     )
