a8dd4e9717a124a7e820e11cb3abbfa7ca393602
wirahadi007 <charlesotojr@gmail.com>
2021-10-17 13:48:32 +0800

    update: fix design login-register

---
+++ client/src/redux/actions/auth.js
@@ -0,0 +1,30 @@
+import axios from '../../axios'
+
+export const authLogin = (data) => {
+    return async dispatch => {
+        try {
+            let result = await axios.post('/users/login' , data)
+            dispatch(setLogin(result.data))
+            return {status: result.status}
+        } catch (error) {
+            return {
+                status: error.response.status,
+                message: error.response.data.message,
+            }
+        }
+    }
+}
+
+export const setLogin = (data) => {
+    const {user , role , subRole, token} = data
+    return {
+        type: "AUTH_LOGIN",
+        payload: {user,role,subRole,token}
+    }
+}
+
+export const setLogout = () => {
+    return {
+        type: "AUTH_LOGOUT",
+    }
+}
