755d6b280c8b4d8a767a57418b2b5faf439e593b
wirahadi007 <charlesotojr@gmail.com>
2021-10-20 16:35:35 +0800

    update: home update

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../../components/home/Navbar'
 import Banner from '../../components/home/Banner'
 import About from '../../components/home/About'
 import Footer from '../../components/home/Footer'
 import Service from '../../components/home/Service'
+import { useDispatch } from 'react-redux'
 
-const index = () => {
+const Home = () => {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch({ type: "FROM_HOME_TRUE" })
+        dispatch({type: "SET_PATH" , payload: {path: "/"}})
+    }, [])
+
     return (
         <div className="pt-14 dark:bg-gray-800 text-gray-600 dark:text-gray-100">
             <Navbar />
@@ -17,4 +24,4 @@ const index = () => {
     )
 }
 
-export default index
+export default Home
