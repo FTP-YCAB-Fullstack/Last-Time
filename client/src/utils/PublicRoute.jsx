e30e225c4bdc8f9962f9e5d690cf0a2140837927
wirahadi007 <charlesotojr@gmail.com>
2021-10-20 16:41:02 +0800

    update: login route update

--- client/src/utils/PublicRoute.jsx
+++ client/src/utils/PublicRoute.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { Redirect , Route } from 'react-router'
 
 const PublicRoute = (props) => {
     const auth = useSelector(state => state.auth)
+    const fromHome = localStorage.getItem('status-from-home')
+    const path = localStorage.getItem('pathname')
+
+    
     return auth.status ?
+            fromHome === 'false' ? <Redirect to={path} /> :
             auth.role === "admin" ?
                 <Redirect to="/admin/dashboard" />
                 : <Redirect to="/user/dashboard" />
